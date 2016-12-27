741849ef17884ef44a6e52c10038ffb3bda13df4
Eduardo <rceduardo93@gmail.com>
2016-12-27 12:45:06 +0000

    Adicionado Packages e utilização de imports

---
+++ Exercicios/Software_banco/src/eduardo/banco/cadastraFuncionarioBanco.java
@@ -0,0 +1,14 @@
+package eduardo.banco;
+
+import eduardo.banco.funcionarios.*;
+
+public class cadastraFuncionarioBanco {
+	
+	public static void main (String[] args){
+		System.out.println("--- Sistema para Cadastro de Funcionários ---");
+		System.out.println("\nCadastrando Funcionário");
+		Funcionario.cadastraFuncionario();
+
+		
+			}
+}
--- Exercicios/Software_banco/src/eduardo/banco/funcionarios/Funcionario.java
+++ Exercicios/Software_banco/src/eduardo/banco/funcionarios/Funcionario.java
@@ -1,8 +1,11 @@
+package eduardo.banco.funcionarios;
+
+
 public class Funcionario {
 	String nome, rg, dpt, dataEntrada;
 	double salario;
-
-	public static void main (String[] args){
+	
+	public static void cadastraFuncionario(){
 		Funcionario f1 = new Funcionario();
 		Funcionario f2 = new Funcionario();
 		f1.nome = "Eduardo"; f1.dataEntrada = "01/12/2016"; f1.rg = "44581202"; f1.dpt = "TI";
@@ -11,10 +14,13 @@ public class Funcionario {
 		f2.nome = "Laíssa"; f2.dataEntrada = "10/12/2016"; f2.rg = "44581205"; f2.dpt = "Direito";
 		f2.salario = 1800.00;
 		f2.mostra();
-
+		System.out.println("\nFuncionários Cadastrados. Será adicionado R$1.000,00 aos salários para adequação");
+		f1.recebeAumento(1000.00);
+		f2.recebeAumento(1000.00);
+		System.out.println("Novos salários: "+f1.nome+": R$"+f1.salario+" e "+f2.nome+": R$"+f2.salario);
 	}
 		
-	void recebeAumento (double valor){
+	public void recebeAumento (double valor){
 		this.salario += valor;
 	}
 	
@@ -23,7 +29,7 @@ public class Funcionario {
 	}
 	
 	void mostra (){
-		System.out.println("O funcionário:\nNome: "+this.nome+"\nRG: "+this.rg+"\nDepartamento: "+this.dpt+"\nData de entrada: "+this.dataEntrada);
+		System.out.println("\nO funcionário:\nNome: "+this.nome+"\nRG: "+this.rg+"\nDepartamento: "+this.dpt+"\nData de entrada: "+this.dataEntrada);
 		System.out.println("\nSalário: R$"+this.salario);
 	}
 	
