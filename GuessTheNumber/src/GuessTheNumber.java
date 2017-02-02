d27f19730a619eab0ebd95c7280766cae7107feb
Eduardo <rceduardo93@gmail.com>
2017-02-02 01:33:53 +0000

    GuessTheNumber FINALIZADO

--- GuessTheNumber/src/GuessTheNumber.java
+++ GuessTheNumber/src/GuessTheNumber.java
@@ -1,3 +1,6 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import javax.swing.JFrame;
 
 
@@ -7,8 +10,9 @@ public class GuessTheNumber {
 	public static void main (String[] args) {
 		Window mainwindow = new Window();
 		
+		
 		mainwindow.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-		mainwindow.setSize (400,200);
+		mainwindow.setSize (350,150);
 		mainwindow.setResizable(false); //Proibe que a tela tenha seu tamanho alterado
 		mainwindow.setLocationRelativeTo(null); //Faz o frame aparecer no meio da tela
 		mainwindow.setVisible(true);
--- GuessTheNumber/src/Window.java
+++ GuessTheNumber/src/Window.java
@@ -4,6 +4,7 @@ import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import java.util.Random;
@@ -11,62 +12,108 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+
 
 public class Window extends JFrame{
-	private JLabel ltitulo, lintervalo, teste;
-	private JTextField intinicio, intfinal;
-	private JButton ok, ttry, newint, exit;
-	String inteinicio, intefinal;
-	
-	public void trywindow(){
-    	
-    	lintervalo.updateUI();
-    	teste = new JLabel();
-	    teste.setText("O intervalo será de");
-	    teste.setText(inteinicio);
-	    //teste.setText(intefinal);
-	    add(teste);
-	    
-    	
-    }
-	
+	private JLabel ltitulo, lintervalo, jogo, showw;
+	private JTextField intinicio, intfinal, chute;
+	private JButton ok, ttry, newint, exit, gerar;
+	int inteinicio, intefinal;
+	private int number;
+
+    
 	public Window(){
 		super("Guess The Number");
-		setLayout (new FlowLayout());
+		setLayout(new FlowLayout());
 		//Adicionando Imagem Título
 		Icon titulo = new ImageIcon ("images\\guessthenumber_title.png");
 		ltitulo = new JLabel (titulo);
 		add(ltitulo);
+		intervaloWindow();
+	}
+	
+	public void intervaloWindow() {
 		//Adicionando Pergunta qual intervalo
 	    lintervalo = new JLabel();
-	    lintervalo.setText("Digite o intervalo[Inicio][Fim]:");
+	    lintervalo.setText("Digite o intervalo[Inicio][Fim]: ");
 	    add(lintervalo);
 	    //Adicionando as caixas de texto
 	    intinicio = new JTextField(5);
 	    add (intinicio);
-	    intfinal = new JTextField(5);
+    	intfinal = new JTextField(5);
 	    add (intfinal);
+    	
 	    //Adicionando botao ok
 	    IntervaloHandler okk = new IntervaloHandler();
 	    ok = new JButton ("OK");
 	    ok.addActionListener(okk);
 	    add (ok);
-			
 	}
 	
+	public void game(){
+		//Descobrir como fazer isso sem ficar usando visible
+		lintervalo.setVisible(false); ok.setVisible(false); intinicio.setVisible(false); intfinal.setVisible(false);
+		
+		//Interface do jogo
+		showw = new JLabel();
+		showw.setText("----------------- O intervalo será de "+inteinicio+" até "+intefinal+" ----------------");
+    	add(showw);
+    	
+    	jogo = new JLabel();
+		jogo.setText("Tentativa: "+number);
+		add (jogo);
+		//Adicionando as caixas de texto
+	    chute = new JTextField(5);
+	    add (chute);
+	    //Adicionando botao try
+	    IntervaloHandler handler = new IntervaloHandler();
+	    ttry = new JButton ("Tentar");
+	    ttry.addActionListener(handler);
+	    add (ttry);
+		gerar = new JButton ("Gerar novo Nº");
+		gerar.addActionListener(handler);
+		add(gerar);
+	}
 	
-    public class IntervaloHandler implements ActionListener {
+	public int generate(int inicio, int finall) {
+        Random gen = new Random();
+        return gen.nextInt(finall) + inicio;
+    }
+	
+    public class IntervaloHandler implements ActionListener  {
         public void actionPerformed (ActionEvent event) {
             if(event.getSource() == ok)
             {
-            	inteinicio = intinicio.getText();
-            	//intefinal = Integer.parseInt (intfinal.getText().trim() );
-            	trywindow();
+            	lintervalo.updateUI();
+        	    inteinicio = Integer.parseInt (intinicio.getText().trim());
+        	    intefinal = Integer.parseInt (intfinal.getText().trim());
+            	number = generate(inteinicio,intefinal);
+            	game();
+            }
+            
+            if (event.getSource() == ttry){
+            	if (Integer.parseInt(chute.getText()) == number){
+            		JOptionPane.showMessageDialog (null, "Você ACERTOU!!");
+            	}
+           		if (Integer.parseInt(chute.getText()) > number){
+               		JOptionPane.showMessageDialog (null, "O número é Menor!");
+               	}
+           		else
+           			if (Integer.parseInt(chute.getText()) < number){
+                   		JOptionPane.showMessageDialog (null, "O número é Maior!");
+                   	}
+            	}
+            
+            if (event.getSource() == gerar){
+            	showw.setVisible(false); jogo.setVisible(false); ttry.setVisible(false); gerar.setVisible(false); chute.setVisible(false);
+            	//jogo.updateUI();
+            	number = generate(inteinicio,intefinal);
+            	game();
             }
         }
+            	
     }
-	
-    
-	
-}
-
+} 
